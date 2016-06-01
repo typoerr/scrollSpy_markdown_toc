a2cd06c3bbf57779bbf2955ade05c0899749520a
cotto89 <cottto89@gmail.com>
2016-06-01 16:43:53 +0900

    setup

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const WebpackNotifierPlugin = require("webpack-notifier");
+const path = require("path");
+const PATHS = {
+  src: path.join(__dirname, "src/scripts"),
+  dist: path.join(__dirname, "dist"),
+  publicPath: "/dist/",
+};
+
+const common = {
+  entry: {
+    main: `${PATHS.src}/index.js`,
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: PATHS.publicPath,
+    filename: "[name].bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+      },
+      {
+        test: /\.vue$/, // a regex for matching all files that end in `.vue`
+        loader: "vue"   // loader to use for matched files
+      },
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.md$/,
+        loader: "raw"
+      }
+    ],
+  },
+  plugins: [
+    new WebpackNotifierPlugin({ title: "Webpack" }),
+    new CopyPlugin([
+      { from: "src/index.html" },
+    ]),
+  ]
+};
+
+const devConf = {
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "dist/",
+    inline: true,
+    hot: true,
+  }
+};
+
+
+const prodConf = {
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+
+    // ライブラリ間で依存しているモジュールが重複している場合、二重に読み込まないようにする
+    new webpack.optimize.DedupePlugin(),
+    // ファイルを細かく分析し、まとめられるところはできるだけまとめてコードを圧縮する
+    new webpack.optimize.AggressiveMergingPlugin(),
+  ]
+};
+
+
+
+module.exports = (() => {
+  const TARGET = process.env.npm_lifecycle_event;
+  let conf = common;
+  if (TARGET === "prod") {
+    conf = merge(common, prodConf);
+  } else {
+    conf = merge(common, devConf);
+  }
+
+  return conf;
+})();
