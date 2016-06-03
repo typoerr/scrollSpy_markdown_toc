e2847c1d88e027ed1722ad2e26277f2e3fc043cc
cotto89 <cottto89@gmail.com>
2016-06-03 22:09:31 +0900

    [add]scroll-spy

--- scripts/components/App.vue
+++ scripts/components/App.vue
@@ -1,13 +1,45 @@
 <template lang="jade">
 .app
-  ul
-    li(v-for="h in headingList") {{h}}
-  div {{{content}}}
+  div.sction.toc
+    ul
+      li(li v-for="(index, tocItem) in headingList" track-by="$index")
+        div(class="tocItem" @click="onTocItemClick(index)"
+         v-bind:class="[index == currentIndex ?    'current': '']")
+          {{tocItem}}
+  div.section.content
+    div {{{content}}}
 </template>
 
 <style scoped>
 .app {
+  position: relative;
   padding: 20px;
+}
+
+.toc {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+
+.tocItem {
+  cursor: pointer;
+  color: #aaa;
+  padding: 10px 5px;
+}
+
+.tocItem:hover {
+  background: #f4f4f4;
+}
+
+.content {
+  margin-left: 150px;
+
+}
+
+.current {
+  color: red;
 }
 </style>
 
--- scripts/components/AppSource.js
+++ scripts/components/AppSource.js
@@ -1,11 +1,14 @@
 import Markdown from "./../lib/md-parser.js";
 import sampleText from "./../../sample.md";
+import scrollToY from "./../lib/scroll-to-y.js";
 
 export default {
   data() {
     return {
       content: "",
       headingList: [],
+      currentIndex: "",
+      scrollItems: [],
     };
   },
 
@@ -17,6 +20,27 @@ export default {
 
 
   ready() {
-    const headers = document.querySelectorAll("[data-toc-heading='true']");
+    const scrollItems = document.querySelectorAll("[data-toc-heading='true']");
+    this.scrollItems = scrollItems;
+
+    window.addEventListener("scroll", () => {
+      const baseTop = window.pageYOffset + 50;
+
+      Array.prototype.forEach.call(scrollItems, (elm, i) => {
+        if (baseTop > elm.offsetTop) {
+          this.currentIndex = i;
+        }
+        if (baseTop === 0) this.currentIndex = 0;
+      });
+    });
+  },
+
+  methods: {
+    onTocItemClick(index) {
+      this.currentIndex = index;
+      const targetY = this.scrollItems[index].offsetTop;
+      scrollToY(targetY, 2000, "easeInOutQuint");
+    }
+
   }
 };
